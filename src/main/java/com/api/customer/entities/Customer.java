bb8187d63cdc2be8eea9b2e88c6965c7ef897d2f
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2023-06-27 15:29:59 -0300

    Pequeno ajuste na entidade customer

--- src/main/java/com/api/customer/entities/Customer.java
+++ src/main/java/com/api/customer/entities/Customer.java
@@ -20,7 +20,7 @@ public class Customer {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	long id;
+	private long id;
 	private String name;
 	private String cpf;
 	private String email;
